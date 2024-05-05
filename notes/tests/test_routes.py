0c216949a7ac49a75efe0d262af8c06684e6acdc
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-05-05 11:55:27 +0300

    Created first test for pages avaibality

---
+++ notes/tests/test_routes.py
@@ -0,0 +1,38 @@
+from http import HTTPStatus
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Читатель простой')
+        cls.note = Note.objects.create(title='1', text='2', author=cls.author)
+
+    def test_pages_availability(self):
+        urls = (
+            ('notes:home', None),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+# url = [('notes:detail', (self.note.slug,)),
+#        ('notes:add', None),
+#        ('notes:edit', (self.note.slug,)),
+#        ('notes:list', None),
+#        ('notes:delete', (self.note.slug,)),
+#        ('notes:success', None),]
