4b31a36cd7b4aa31ffcf0d1ac5944a46e5bacc92
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-05-05 14:49:31 +0300

    Created conftest.py for app notes and discribed fixture for Client

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,17 @@
+# test_routes.py
+import pytest
+from http import HTTPStatus
+
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
